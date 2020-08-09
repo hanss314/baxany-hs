a9b38b9a9e4a255374065a7189350acaea2c763b
hanss314 <hanss314@gmail.com>
2020-08-09 19:57:14 -0400

    Implement move getters for standard steppers and doers for all standards

--- Move.hs
+++ Move.hs
@@ -16,9 +16,13 @@ mb Black = map (\(x,y)->(x,-y))
 mmb :: Color -> [Move] -> [Move]
 mmb c = map (mb c)
 
+listify :: a -> [a]
+listify x = [x]
+
 -- capturer capturee
 canCapture :: Piece -> Piece -> Bool
 canCapture (Piece c _) (Piece b _) = c /= b
+canCapture _ Empty = True
 canCapture _ _ = False
 
 -- capturee capturee capturer
@@ -28,16 +32,13 @@ preCapture _ _ _ b = b
 postCapture :: Piece -> Pos -> Board -> Board
 postCapture _ _ b = b
 
-normalMove :: Board -> Pos -> Pos -> Board
-normalMove b start end = case getPiece b end of
-    Block -> b
-    Empty -> rawMovePiece start end b
-    x -> b & preCapture x end start & rawMovePiece start end & postCapture x end
-
 getEps :: Color -> Board -> Pos -> [Pos]
 getEps c board pos@(x,y) = filter (canEp pos . getPiece board) $ map ((,) x) [0..(size board)-1] where
     canEp movepos (Piece pc (Pawn (EnPassant eppos))) = c /= pc && elem movepos eppos
     canEp _ _ = False
+
+basicFilter :: Board -> Piece -> [Pos] -> [Move]
+basicFilter b p = (map listify . filter (canCapture p . getPiece b))
 
 getMoves :: Board -> Piece -> Pos -> [Move]
 -- Pawn
@@ -47,7 +48,11 @@ getMoves b pawn@(Piece c (Pawn m)) p = forward ++ second ++ captures ++ eps wher
             filter ((==) Empty . getPiece b . head) $ map ((p >+) . mb c) [[(0,2)]]
     captures = filter (canCapture pawn . getPiece b . head) $ map ((p >+) . mb c) [[(1,1)], [(-1,1)]]
     eps = filter (not . null . getEps c b . head) $ map ((p >+) . mb c) [[(1,1)], [(-1,1)]]
-    
+
+-- Rest of normal chess pieces
+getMoves b king@(Piece _ King) p = basicFilter b king $ p >+ ([(0,1),(1,1)] >>= r4)
+getMoves b knight@(Piece _ Knight) p = basicFilter b knight $ p >+ (mh (1,2) >>= r4)
+
 -- default piece has no moves
 getMoves _ _ _ = []
 
@@ -57,6 +62,12 @@ movesAt b pos = getMoves b (getPiece b pos) pos
 capturingMove :: Pos -> Pos -> Board -> Board
 capturingMove s e b = b & preCapture p e s & rawMovePiece s e & postCapture p e where
     p = getPiece b e
+
+normalMove :: Pos -> Pos -> Board -> Board
+normalMove start end b = case getPiece b end of
+    Block -> b
+    Empty -> rawMovePiece start end b
+    x -> b & preCapture x end start & rawMovePiece start end & postCapture x end
 
 nextPawn :: Piece -> [Pos] -> Piece
 nextPawn (Piece c (Pawn Start)) eps = (Piece c (Pawn (EnPassant eps)))
@@ -74,6 +85,13 @@ doMove b pawn@(Piece c (Pawn _)) s@(x,y) m@[e@(mx,my)]
         epCapPie = getPiece b epCap
         myeps = [(x,j) | j <- [(min y my)+1..(max y my)-1]]
         normalmove = rawSetPieces $ (e,nextPawn pawn myeps) : (s,Empty) :  map (flip (,) Empty) epsPos
+
+-- Regular chess pieces
+doMove b (Piece _ King) s [e] = normalMove s e b
+doMove b (Piece _ Knight) s [e] = normalMove s e b
+doMove b (Piece _ Queen) s [e] = normalMove s e b
+doMove b (Piece _ Bishop) s [e] = normalMove s e b
+doMove b (Piece _ Rook) s [e] = normalMove s e b
 
 doMove b _ _ _ = b
 
