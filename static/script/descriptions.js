ea6208442eaeda37bd4acccd0b15e646742eb924
hanss314 <hanss314@gmail.com>
2020-08-21 13:09:00 -0400

    Fix typo

---
+++ static/script/descriptions.js
@@ -0,0 +1,48 @@
+const titles = [
+    "King", "Lance", "Pao", "Mage", "Kangaroo", "Jack",
+    "Queen", "Hook Mover", "Cardinal", "Giraffe", "Lion", "Trebuchet",
+    "Rook", "Joker", "Chancellor", "Zebra", "Chariot", "Empress",
+    "Bishop", "Amazon", "Dragon", "Camel", "Elephant", "Ghoul",
+    "Knight", "Knightmare", "Vao", "Cobra", "Chameleon", "Gryphon",
+    "Pawn", "General", "Leo", "Imitator", "Ace"
+]
+
+const descriptions = [
+    "Regular chess king",
+    "The lance moves rook but only forwards. <br /> Promotes to a rook upon reaching the back rank." ,
+    "Regular chinese chess cannon",
+    "Moves like a knight. <br /> Grants a knight move to friendly pieces within a king's move",
+    "Moves like a knight but makes a step of (4,3)",
+    "Can move anywhere within two squares. <br /> Can wrap around the sides of the board",
+    "Regular chess queen",
+    "Can make two bishop moves, but move ends on capture",
+    "Moves as bishop or knight",
+    "Moves like a knight but makes a step of (4,2)",
+    "Makes two king moves.",
+    "Moves as an elephant. <br /> Can throw an orthogonally adjacent piece forwards up to 5 squares.",
+    "Regular chess rook",
+    "Makes two knightmare moves",
+    "Moves as rook or knight",
+    "Moves like a knight but makes a step of (3,2)",
+    "Moves like a queen but cannot capture. <br /> Can carry an adjacent friendly piece and capture with that piece.",
+    "Makes two queen moves.",
+    "Regular chess bishop",
+    "Moves like queen or knight",
+    "Moves as rook or lion",
+    "Moves like a knight but makes a step of (3,1)",
+    `Can make a king move in any direction. <br /> It will push pieces, regardless of color. 
+                                            <br /> Pieces pushed off the board are captured.`, 
+    "Can move to any unoccupied square. <br /> Any piece capturing it is captured in return.",
+    "Regular chess knight",
+    "Can make any number of knight moves in one direction",
+    "Works like a pao on diagonals",
+    "Moves like a knight but with a step of (2,2)",
+    "Moves like the piece the opponent last moved",
+    `Can move and capture anywhere within two kings moves. <br /> 
+     May push an enemy piece exactly 3 squares orthogonal or diagonal up to 3 squares`,
+    "Normal chess pawn. May move 3 squares on first turn. <br /> En Passant is only possible when it moves 3 squares",
+    "Moves like a king. <br /> Can swap with the king within two squares.",
+    "Moves as a pao or a vao",
+    "Moves like a queen. <br /> Can only capture a piece threatening it",
+    "Moves like a king. <br /> Promotes to joker if it captures 10 non-pawn pieces or the empress"
+]
